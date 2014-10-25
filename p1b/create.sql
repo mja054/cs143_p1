5399aa030bf82339b20d0d10af2cebbeaed23276
Spencer Tung <spencer.zh.tung@gmail.com>
2014-10-25 04:10:57 -0700

    Some tables loaded, primary key constraints failing on MovieGenre

--- p1b/create.sql
+++ p1b/create.sql
@@ -1,7 +1,10 @@
-CREATE TABLE Movie (id int, title varchar(100), year int, rating varchar(10), company varchar(50));
-CREATE TABLE Actor (id int, last varchar(20), first varchar(20), sex varchar(6), dob date, dod date);
-CREATE TABLE Director (id int, last varchar(20), first varchar(20), dob date, dod date);
-CREATE TABLE MovieGenre (mid int, genre varchar(20));
-CREATE TABLE MovieDirector (mid int, did int);
-CREATE TABLE MovieActor (mid int, aid int, role varchar(50));
-CREATE TABLE Review (name varchar(20), time timestamp, mid int, rating int, comment varchar(500));
+CREATE TABLE IF NOT EXISTS Movie (id int PRIMARY KEY, title varchar(100), year int, rating varchar(10), company varchar(50));
+CREATE TABLE IF NOT EXISTS Actor (id int PRIMARY KEY, last varchar(20), first varchar(20), sex varchar(6), dob date, dod date);
+CREATE TABLE IF NOT EXISTS Director (id int PRIMARY KEY, last varchar(20), first varchar(20), dob date, dod date);
+CREATE TABLE IF NOT EXISTS MovieGenre (mid int PRIMARY KEY, genre varchar(20));
+CREATE TABLE IF NOT EXISTS MovieDirector (mid int PRIMARY KEY, did int);
+CREATE TABLE IF NOT EXISTS MovieActor (mid int PRIMARY KEY, aid int, role varchar(50));
+CREATE TABLE IF NOT EXISTS Review (name varchar(20), time timestamp, mid int, rating int, comment varchar(500));
+
+CREATE TABLE IF NOT EXISTS MaxPersonID (id int);
+CREATE TABLE IF NOT EXISTS MaxMovieID (id int);
