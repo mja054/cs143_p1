40c3e24dc30531418c47ac32a96969afa1ff82ad
Spencer Tung <spencer.zh.tung@gmail.com>
2014-10-25 15:30:41 -0700

    Completed two queries, added truncate to load and drop to create to be safe

--- p1b/create.sql
+++ p1b/create.sql
@@ -1,9 +1,17 @@
+DROP TABLE IF EXISTS Movie;
+DROP TABLE IF EXISTS Actor;
+DROP TABLE IF EXISTS Director;
+DROP TABLE IF EXISTS MovieGenre;
+DROP TABLE IF EXISTS MovieDirector;
+DROP TABLE IF EXISTS MovieActor;
+DROP TABLE IF EXISTS Review;
+
 CREATE TABLE IF NOT EXISTS Movie (id int PRIMARY KEY, title varchar(100), year int, rating varchar(10), company varchar(50));
 CREATE TABLE IF NOT EXISTS Actor (id int PRIMARY KEY, last varchar(20), first varchar(20), sex varchar(6), dob date, dod date);
 CREATE TABLE IF NOT EXISTS Director (id int PRIMARY KEY, last varchar(20), first varchar(20), dob date, dod date);
-CREATE TABLE IF NOT EXISTS MovieGenre (mid int PRIMARY KEY, genre varchar(20));
-CREATE TABLE IF NOT EXISTS MovieDirector (mid int PRIMARY KEY, did int);
-CREATE TABLE IF NOT EXISTS MovieActor (mid int PRIMARY KEY, aid int, role varchar(50));
+CREATE TABLE IF NOT EXISTS MovieGenre (mid int, genre varchar(20));
+CREATE TABLE IF NOT EXISTS MovieDirector (mid int, did int);
+CREATE TABLE IF NOT EXISTS MovieActor (mid int, aid int, role varchar(50));
 CREATE TABLE IF NOT EXISTS Review (name varchar(20), time timestamp, mid int, rating int, comment varchar(500));
 
 CREATE TABLE IF NOT EXISTS MaxPersonID (id int);
---
+++ p1b/queries.sql
@@ -0,0 +1,11 @@
+-- Names of actors in the movie 'Die Another Day'
+SELECT CONCAT(first, ' ', last)
+FROM Actor A, MovieActor MA, Movie M
+WHERE A.id=MA.aid AND MA.mid=M.id AND M.title='Die Another Day';
+
+-- Count of all actors who acted in multiple movies
+SELECT COUNT(*)
+FROM (SELECT aid
+	  FROM MovieActor MA
+	  GROUP BY aid
+	  HAVING COUNT(aid) > 1) AS A;
