b8eafc4486d88cf4942f6193c34e7e1e5c2995ee
Mohammed Junaid <mdjunaid.54@gmail.com>
2014-11-06 19:05:49 -0800

    Updated I2.php, execute command needs to be implemented.

--- p1c/src/I2.php
+++ p1c/src/I2.php
@@ -4,42 +4,42 @@
 <body>
 <h1>Add a new movie here!</h1>
 
-<?php
-?>
-
 <form action="I2.php" method="GET">
-<textarea name="query" rows="8" cols="60"><?php echo $_GET["query"];?></textarea>
 Title<input type="text" name="title" maxlength="20"><br />
-Compnay: <input type="text" name="company" maxlength="50"><br/>
+Company: <input type="text" name="company" maxlength="50"><br/>
 Year : <input type="text" name="year" maxlength="4"><br/>	<!-- Todo: validation-->
 MPAA Rating : <select name="mpaarating">
-     	      	      <option value="G">G</option>
+		      <option value="G">G</option>
 		      <option value="NC-17">NC-17</option>
 		      <option value="PG">PG</option>
 		      <option value="PG-13">PG-13</option>
 		      <option value="R">R</option>
 		      <option value="surrendere">surrendere</option>
 		</select><br/>
-Genre :
-<input type="checkbox" name="genre_Action" value="Action">Action</input>
-<input type="checkbox" name="genre_Adult" value="Adult">Adult</input>
-<input type="checkbox" name="genre_Adventure" value="Adventure">Adventure</input>
-<input type="checkbox" name="genre_Animation" value="Animation">Animation</input>
-<input type="checkbox" name="genre_Comedy" value="Comedy">Comedy</input>
-<input type="checkbox" name="genre_Crime" value="Crime">Crime</input>
-<input type="checkbox" name="genre_Documentary" value="Documentary">Documentary</input>
-<input type="checkbox" name="genre_Drama" value="Drama">Drama</input>
-<input type="checkbox" name="genre_Family" value="Family">Family</input>
-<input type="checkbox" name="genre_Fantasy" value="Fantasy">Fantasy</input>
-<input type="checkbox" name="genre_Horror" value="Horror">Horror</input>
-<input type="checkbox" name="genre_Musical" value="Musical">Musical</input>
-<input type="checkbox" name="genre_Mystery" value="Mystery">Mystery</input>
-<input type="checkbox" name="genre_Romance" value="Romance">Romance</input>
-<input type="checkbox" name="genre_Sci-Fi" value="Sci-Fi">Sci-Fi</input>
-<input type="checkbox" name="genre_Short" value="Short">Short</input>
-<input type="checkbox" name="genre_Thriller" value="Thriller">Thriller</input>
-<input type="checkbox" name="genre_War" value="War">War</input>
-<input type="checkbox" name="genre_Western" value="Western">Western</input>
+Genre :<br />
+<input type="checkbox" name="moviegenre[]" value="Action">Action</input>
+<input type="checkbox" name="moviegenre[]" value="Adult">Adult</input>
+<input type="checkbox" name="moviegenre[]" value="Adventure">Adventure</input>
+<input type="checkbox" name="moviegenre[]" value="Animation">Animation</input>
+<br />
+<input type="checkbox" name="moviegenre[]" value="Comedy">Comedy</input>
+<input type="checkbox" name="moviegenre[]" value="Crime">Crime</input>
+<input type="checkbox" name="moviegenre[]" value="Documentary">Documentary</input>
+<input type="checkbox" name="moviegenre[]" value="Drama">Drama</input>
+<br />
+<input type="checkbox" name="moviegenre[]" value="Family">Family</input>
+<input type="checkbox" name="moviegenre[]" value="Fantasy">Fantasy</input>
+<input type="checkbox" name="moviegenre[]" value="Horror">Horror</input>
+<input type="checkbox" name="moviegenre[]" value="Musical">Musical</input>
+<br />
+<input type="checkbox" name="moviegenre[]" value="Mystery">Mystery</input>
+<input type="checkbox" name="moviegenre[]" value="Romance">Romance</input>
+<input type="checkbox" name="moviegenre[]" value="Sci-Fi">Sci-Fi</input>
+<input type="checkbox" name="moviegenre[]" value="Short">Short</input>
+<br />
+<input type="checkbox" name="moviegenre[]" value="Thriller">Thriller</input>
+<input type="checkbox" name="moviegenre[]" value="War">War</input>
+<input type="checkbox" name="moviegenre[]" value="Western">Western</input>
 <br/>
 <input type = "submit" name = "submit">
 </form>
@@ -47,51 +47,107 @@ Genre :
 <?php
   include "db_base.php";
 
-  function execute_command()
-  {
-    $input_pattern = '/^(select|show)/i';
-    $user_query = $_GET["query"];
+  class add_movie {
+    var $title;
+    var $company;
+    var $year;
+    var $mpaa_rating;
+    var $movieGenre;
 
-    if ($user_query == '') {
-      die();
-    } else if (!preg_match($input_pattern, $user_query)) {
-      die('<h4>Only select/show commands are supported.</h4>');
+    function add_movie()
+    {
+	$this->title = '';
+	$this->company = '';
+	$this->year = '';
+	$this->mpaa_rating = '';
+	$this->movieGenre = '';
     }
 
-    $db_con = new dbConnect();
-    $rs = $db_con->execute_command($user_query);
-    if (!$rs) {
-       die('<h4>Invalid query: ' . mysql_error() . '</h4>');
+    function parse_input()
+    {
+	$this->title = $_GET["title"];
+	$this->company = $_GET["company"];
+	$this->year = (int)$_GET["year"];
+	$this->mpaa_rating = $_GET["mpaarating"];
+	$this->movieGenre = $_GET["moviegenre"];
     }
 
-    print "<h3>Results from MySQL:</h3>";
-    print "<table border=1 cellspacing=1 cellpadding=2>";
-    print "<tr align=center>";
-    for ($i = 0; $i < $db_con->get_num_fields($rs); $i++) {
-        print "<td>";
-        print $db_con->get_field_name($rs, $i);
-	print "</td>";
-    }
-    print "</tr>";
-
-    while ($row = $db_con->fetch_row($rs)) {
-      print "<tr align=center>";
-      foreach ($row as $rv) {
-        print "<td>";
-	if ($rv == "") {
-	  print "N/A";
-	} else {
-	  print $rv;
+    function debug_input()
+    {
+	echo "title: $this->title <br />";
+	echo "company: $this->company <br />";
+	echo "year: $this->year <br />";
+	echo "mpaarating: $this->mpaa_rating <br />";
+	echo "movieGenre: <br />";
+	$movieGenre = $this->movieGenre;
+	for($i=0; $i < count($this->movieGenre); $i++)
+	{
+	    echo "$movieGenre[$i] <br /> ";
 	}
-	print "</td>";
-      }
-      print "</tr>";
     }
-    print "</table>";
+
+    function validate()
+    {
+	if (empty($this->title)) {
+	    die("Movie title cannot be empty.");
+	} else if (empty($this->company)) {
+	    die("Movie company cannot be empty.");
+	} else if (empty($this->year) ||
+		   !(($this->year > 1700) && ($this->year <= (int)date("Y")))) {
+	    die("Movie year cannot be empty or is invalid.");
+	} else if (count($this->movieGenre) == 0) {
+	    die("Please select atleast one genre for the movie.");
+	}
+    }
+
+    function parse_and_validate()
+    {
+	$this->parse_input();
+	$this->validate();
+    }
+
+    function execute_command()
+    {
+/*
+	$db_con = new dbConnect();
+	$rs = $db_con->execute_command($user_query);
+	if (!$rs) {
+	    die('<h4>Invalid query: ' . mysql_error() . '</h4>');
+	}
+
+	print "<h3>Results from MySQL:</h3>";
+	print "<table border=1 cellspacing=1 cellpadding=2>";
+	print "<tr align=center>";
+	for ($i = 0; $i < $db_con->get_num_fields($rs); $i++) {
+	    print "<td>";
+	    print $db_con->get_field_name($rs, $i);
+	    print "</td>";
+	}
+	print "</tr>";
+
+	while ($row = $db_con->fetch_row($rs)) {
+	    print "<tr align=center>";
+	    foreach ($row as $rv) {
+		print "<td>";
+		if ($rv == "") {
+		    print "N/A";
+		} else {
+		    print $rv;
+		}
+		print "</td>";
+	    }
+	    print "</tr>";
+	}
+	print "</table>";
+*/
+    }
   }
 
   if (isset($_GET["submit"])) {
-     execute_command();
+     $movieObj = new add_movie();
+     $movieObj->parse_and_validate();
+//     $movieObj->debug_input();
+     $movieObj->execute_command();
   }
 ?>
 
