35d60c9761869e8867555e6d1835f7d2e7eaf1e8
Spencer Tung <spencer.zh.tung@gmail.com>
2014-11-08 10:50:11 -0800

    Adding the html part of search

--- p1c/src/search.php
+++ p1c/src/search.php
@@ -1,68 +1,60 @@
 <DOCTYPE html>
 <html>
-<title> CS143 Project 1b </title>
+<title> Search for Actor/Movies! </title>
 <body>
-<h1>Type an SQL query in the following box</h1>
+<h1>Search for your favorite actor or movie here!</h1>
 
-<?php
-?>
-
-<form action="query.php" method="GET">
-<textarea name="query" rows="8" cols="60"><?php echo $_GET["query"];?></textarea>
-<input type = "submit" name = "submit">
+<form action="search.php" method="GET">
+  Enter your search query here: <textarea name="query" rows="1" cols="10"><?php echo $_GET["query"];?></textarea>
+  <input type = "submit" name = "submit">
 </form>
 
-<?php
-  function connectDB()
-  {
-    $db_connection = mysql_connect("localhost","cs143", "");
-    mysql_select_db("CS143", $db_connection);
-    return $db_connection;
-  }
 
+<?php
+  include "db_base.php";
   function execute_command()
   {
-    $input_pattern = '/^(select|show)/i';
-    $user_query = $_GET["query"];
+    $query = $_GET["query"];
 
-    if ($user_query == '') {
-      die();
-    } else if (!preg_match($input_pattern, $user_query)) {
-      die('<h4>Only select/show commands are supported.</h4>');
+    $db_con = new dbConnect();
+
+    # Crafting the search query as a SQL query
+
+
+    # ================================================
+
+
+
+    $searchQuery = "SELECT id FROM MaxPersonID";
+    $querySearch = $db_con->execute_command($searchQuery);
+    while ($row = $db_con->fetch_row($querySearch)) {
+      $newID = $row[0];
     }
 
-    $db_connection = connectDB();
+    # Updating the max ID
+    $updateMaxIDquery = "UPDATE MaxPersonID SET id=id+1";
+    $updateMaxID = $db_con->execute_command($updateMaxIDquery);
 
-    $rs = mysql_query($user_query, $db_connection);
-    if (!$rs) {
-       die('<h4>Invalid query: ' . mysql_error() . '</h4>');
+    if ($actDir == "Actor") {
+      $insert_query = "INSERT INTO Actor VALUES($newID, '$last', '$first', '$sex', $dob, $dod)";
+    }
+    else if ($actDir == "Director") {
+      $insert_query = "INSERT INTO Director VALUES($newID, '$last', '$first', $dob, $dod)";
+    }
+    
+    $insertDB = $db_con->execute_command($insert_query);
+
+    if (!$insertDB) {
+      $error = mysql_error();
+      $updateMaxIDquery = "UPDATE MaxPersonID SET id=id-1";
+      $updateMaxID = $db_con->execute_command($updateMaxIDquery);
+      die("Invalid query: $error");
+    }
+    else {
+      print "Successfully inserted $first $last into the $actDir database!";
     }
 
-    print "<h3>Results from MySQL:</h3>";
-    print "<table border=1 cellspacing=1 cellpadding=2>";
-    print "<tr align=center>";
-    for ($i = 0; $i < mysql_num_fields($rs); $i++) {
-        print "<td>";
-        print mysql_field_name($rs, $i);
-	print "</td>";
-    }
-    print "</tr>";
-
-    while ($row = mysql_fetch_row($rs)) {
-      print "<tr align=center>";
-      foreach ($row as $rv) {
-        print "<td>";
-	if ($rv == "") {
-	  print "N/A";
-	} else {
-	  print $rv;
-	}
-	print "</td>";
-      }
-      print "</tr>";
-    }
-    print "</table>";
-    mysql_close($db_connection);
+    $db_con->close_db();
   }
 
   if (isset($_GET["submit"])) {
