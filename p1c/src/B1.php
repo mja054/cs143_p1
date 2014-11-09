fb5d8dabdbb6fd3080a0bfdbe7d410b23b4ad2a3
Mohammed Junaid <mdjunaid.54@gmail.com>
2014-11-09 11:23:53 -0800

    updated B1/2 to display all the required information.

--- p1c/src/B1.php
+++ p1c/src/B1.php
@@ -36,12 +36,33 @@
     $result_q = print_movie_list($db_conn, $Mresult);
     return $result_q ? true : false;
   }
+
+  function search_actor_info($db_con, $aid)
+  {
+    $q1 = "Select id,first,last, sex, dob,dod from Actor where id=".$aid;
+
+    $res = $db_con->execute_command($q1) or die ("<h3>" . mysql_errno() . " : " . mysql_error() . "</h3>");
+
+    $row = $db_con->fetch_row($res);
+    echo "<br />";
+    echo "Name: " . $row[1] . " " . $row[2] . "<br />";
+    echo "Sex: " . $row[3] . "<br />";
+    echo "Date of Birth: " . $row[4] . "<br />";
+    if ($row[5]) {
+      echo "Date of Death: " . $row[5] . "<br />";
+    } else {
+      echo "Date of Death: Still alive<br />";
+    }
+  }
+
   function execute_command()
   {
     $aid = $_GET["id"];
     $db_con = new dbConnect();
+    search_actor_info($db_con, $aid);
     $query = "SELECT mid FROM MovieActor WHERE aid=".$aid;
     $res = $db_con->execute_command($query) or die("<h3>" . mysql_errno() . " : " . mysql_error() . "</h3>");
+    echo "<br /> Acted in the follwing movies<br />";
     while($row=$db_con->fetch_row($res)) {
       search_movie_info($db_con, $row[0]);
     }
--- p1c/src/B2.php
+++ p1c/src/B2.php
@@ -16,38 +16,107 @@
 <?php
   include "db_base.php";
 
-  function print_actor_list($db_con, $Ares)
+  function print_actor_list($db_con, $Ares, $role)
   {
     $i = 0;
     echo "<table>";
     echo "<tbody>";
     while ($row = $db_con->fetch_row($Ares)) {
-      echo "<tr><td><a href=\"./B1.php?id=".$row[0]."\">".$row[1]." ".$row[2]." (".$row[3].")</a></td></tr>";
+      echo "<tr><td><a href=\"./B1.php?id=".$row[0]."\">".$row[1]." ".$row[2]." (".$row[3].")</a> as " . $role . "</td></tr>";
       $i = $i+1;
     }
     echo "</tbody></table>";
     return $i;
   }
 
-  function search_actor_info($db_con, $aid)
+  function search_actor_info($db_con, $aid, $role)
   {
     $q1 = "Select id,first,last,dob,dod from Actor where id=".$aid;
 
     $Aresult = $db_con->execute_command($q1) or die ("<h3>" . mysql_errno() . " : " . mysql_error() . "</h3>");
-    $result_q3 = print_actor_list($db_con, $Aresult);
+    $result_q3 = print_actor_list($db_con, $Aresult, $role);
 
     return (result_q3) ? true : false;
+  }
+
+  function print_director($db_con, $did)
+  {
+    $mq1 = "Select first,last from Director where id=".$did;
+    $Mresult = $db_con->execute_command($mq1) or die ("<h3>" . mysql_errno() . " : " . mysql_error() . "</h3>");
+    $drow = $db_con->fetch_row($Mresult);
+    echo "Director: " . $drow[0] . " " . $drow[1] . "<br />";
+  }
+
+  function print_movie_director($db_con, $mid)
+  {
+    $mq1 = "select did from MovieDirector where mid=".$mid;
+    $Mresult = $db_con->execute_command($mq1) or die ("<h3>" . mysql_errno() . " : " . mysql_error() . "</h3>");
+    $mdrow = $db_con->fetch_row($Mresult);
+    print_director($db_con, $mdrow[0]);
+  }
+
+  function print_movie_genre($db_con, $mid)
+  {
+    $mq1 = "select genre from MovieGenre where mid=".$mid;
+    $Mresult = $db_con->execute_command($mq1) or die ("<h3>" . mysql_errno() . " : " . mysql_error() . "</h3>");
+    echo "Genre:";
+    while($grow = $db_con->fetch_row($Mresult)) {
+      echo " " . $grow[0];
+    }
+    echo "<br />";
+  }
+
+  function search_movie_info($db_con, $mid)
+  {
+    $mq1 = "select id,title,year,company,rating from Movie where id=".$mid;
+
+    $Mresult = $db_con->execute_command($mq1) or die ("<h3>" . mysql_errno() . " : " . mysql_error() . "</h3>");
+    $row = $db_con->fetch_row($Mresult);
+    echo "<br />";
+    echo "Title: " . $row[1] . " (" . $row[2] . ")<br />";
+    echo "Producer: ". $row[3] . "<br />";
+    echo "MPAA rating: " . $row[4] . "<br />";
+    print_movie_director($db_con, $mid);
+    print_movie_genre($db_con, $mid);
+  }
+
+  function print_movie_rating($db_con, $mid)
+  {
+    $mq1 = "select AVG(rating), COUNT(*) from Review where mid=".$mid;
+
+    $Mresult = $db_con->execute_command($mq1) or die ("<h3>" . mysql_errno() . " : " . mysql_error() . "</h3>");
+    $row = $db_con->fetch_row($Mresult);
+    echo "Average rating: " . $gow[0] . " by " . $row[1] . " users";
+  }
+
+  function print_movie_review($db_con, $mid)
+  {
+    echo "<br />User Reviews<br />";
+    print_movie_rating($db_con, $mid);
+
+    $mq1 = "select time, name, rating, comment from Review where mid=".$mid;
+
+    $Mresult = $db_con->execute_command($mq1) or die ("<h3>" . mysql_errno() . " : " . mysql_error() . "</h3>");
+    while($row = $db_con->fetch_row($Mresult)) {
+      echo "In " + $row[0] . ", " . $row[1] . " rates this movie " . $row[2] . "<br />";
+      echo "Comment: '" . $row[3] . "'<br />";
+    }
   }
   function execute_command()
   {
     $mid = $_GET["id"];
     $db_con = new dbConnect();
-    $query = "SELECT aid FROM MovieActor WHERE mid=".$mid;
+    search_movie_info($db_con, $mid);
+
+    echo "<br />Actors in the Movie<br />";
+    $query = "SELECT aid,role FROM MovieActor WHERE mid=".$mid;
     $res = $db_con->execute_command($query) or die("<h3>" . mysql_errno() . " : " . mysql_error() . "</h3>");
     while($row=$db_con->fetch_row($res)) {
-      search_actor_info($db_con, $row[0]);
+      search_actor_info($db_con, $row[0], $row[1]);
     }
+    print_movie_review($db_con, $mid);
     $db_con->close_db();
+    echo "<br /><a href=\"./I3.php?movie=".$mid ."\">Add comment</a><br />";
   }
 
   if (isset($_GET["id"])) {
